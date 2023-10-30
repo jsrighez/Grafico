d3a7322c32f9cff064e4376f28a5dcd8ba1cd82a
jsrighez <130395388+jsrighez@users.noreply.github.com>
2023-10-30 15:12:34 -0300

    Update run.py

--- run.py
+++ run.py
@@ -32,32 +32,44 @@ class Dados_Grafico:
 
 #Precisa desses?
 class Grafico_coluna(Dados_Grafico):
-    def __init__(self,tituloc_d1,tituloc_d2):
+    def __init__(self,tituloc_d1,tituloc_d2,dgcodc):
         self.tituloc_d1 = tituloc_d1
         self.tituloc_d2 = tituloc_d2
+        self.dgcodc = dgcodc
 
 class Grafico_barra(Dados_Grafico):
-    def __init__(self,titulob_d1,titulob_d2):
+    def __init__(self,titulob_d1,titulob_d2,dgcodb):
         self.titulob_d1 = titulob_d1
         self.titulob_d2 = titulob_d2
+        self.dgcodb = dgcodb
+
 
 class Grafico_pizza(Dados_Grafico):
-    def __init__(self,titulop_d1,titulop_d2):
+    def __init__(self,titulop_d1,titulop_d2,dgcodp):
         self.titulop_d1 = titulop_d1
         self.titulop_d2 = titulop_d2
+        self.dgcodp = dgcodp
 
 #Apenas uma coluna
 
 class Grafico_coluna1(Dados_Grafico):
-    def __init__(self,tituloc_d):
+    def __init__(self,tituloc_d, col1cod,dgcodc1):
         self.tituloc_d = tituloc_d
+        self.col1cod = col1cod
+        self.dgcodc1 = dgcodc1
 
 class Grafico_barra1(Dados_Grafico):
-    def __init__(self,titulob_d):
+    def __init__(self,titulob_d,bar1cod,dgcodb1):
         self.titulob_d = titulob_d
+        self.bar1cod = bar1cod
+        self.dgcodb1 = dgcodb1
+
+
 class Grafico_pizza1(Dados_Grafico):
-    def __init__(self,titulop_d):
+    def __init__(self,titulop_d,piz1cod,dgcodp1):
         self.titulop_d = titulop_d
+        self.piz1cod = piz1cod
+        self.dgcodp1 = dgcodp1
         
 
 def create_table():
@@ -80,7 +92,7 @@ def create_table():
             )
         """)
         conn.commit()
-        print("Tabela criada com sucesso!")
+        print("Tabela usuário criada com sucesso!")
 
         # Cria a tabela
         x = conn.cursor()
@@ -92,7 +104,7 @@ def create_table():
             )
         """)
         conn.commit()
-        print("Tabela Processo criada com sucesso!")
+        print("Tabela Arquivo criada com sucesso!")
 
         # Cria a tabela post
         y = conn.cursor()
@@ -111,7 +123,7 @@ def create_table():
             )
         """)
         conn.commit()
-        print("Tabela Post criada com sucesso!")
+        print("Tabela Dados_Grafico criada com sucesso!")
 
         z = conn.cursor()
         z.execute("""
@@ -125,7 +137,7 @@ def create_table():
           )
         """)
         conn.commit()
-        print("Tabela Post criada com sucesso!")
+        print("Tabela Grafico_coluna criada com sucesso!")
 
         a = conn.cursor()
         a.execute("""
@@ -139,13 +151,13 @@ def create_table():
             )
         """)
         conn.commit()
-        print("Tabela Post criada com sucesso!")
+        print("Tabela Grafico_barra criada com sucesso!")
 
         b= conn.cursor()
         b.execute("""
             CREATE TABLE IF NOT EXISTS Grafico_pizza (
                 pizcod INTEGER NOT NULL,
-                titulocp_d1 VARCHAR(50),
+                titulop_d1 VARCHAR(50),
                 titulop_d2 VARCHAR(50),
                 dgcodp INTEGER,
                 PRIMARY KEY(pizcod,dgcodp),
@@ -153,7 +165,7 @@ def create_table():
             )
         """)
         conn.commit()
-        print("Tabela Processo criada com sucesso!")
+        print("Tabela Grafico_pizza criada com sucesso!")
 
         c= conn.cursor()
         c.execute("""
@@ -166,33 +178,33 @@ def create_table():
             )
         """)
         conn.commit()
-        print("Tabela Processo criada com sucesso!")
+        print("Tabela Grafico_coluna1 criada com sucesso!")
 
         d= conn.cursor()
         d.execute("""
             CREATE TABLE IF NOT EXISTS Grafico_barra1 (
                 bar1cod INTEGER NOT NULL,
-                tituloc_d1 VARCHAR(50),
+                titulob_d1 VARCHAR(50),
                 dgcodb1 INTEGER,
                 PRIMARY KEY(bar1cod,dgcodb1),
                 FOREIGN KEY(dgcodb1) REFERENCES Dados_Grafico(dgcod)
             )
         """)
         conn.commit()
-        print("Tabela Processo criada com sucesso!")
+        print("Tabela Grafico_barra1 criada com sucesso!")
 
         e= conn.cursor()
         e.execute("""
             CREATE TABLE IF NOT EXISTS Grafico_pizza1 (
                 piz1cod INTEGER NOT NULL,
-                tituloc_d1 VARCHAR(50),
+                titulop_d1 VARCHAR(50),
                 dgcodp1 INTEGER,
                 PRIMARY KEY(piz1cod,dgcodp1),
                 FOREIGN KEY(dgcodp1) REFERENCES Dados_Grafico(dgcod)
             )
         """)
         conn.commit()
-        print("Tabela Processo criada com sucesso!")
+        print("Tabela Grafico_pizza1 criada com sucesso!")
 
     except psycopg2.Error as e:
         print("Erro ao criar tabela:", e)
@@ -238,19 +250,19 @@ def insert_arquivo(arq):
 
         # Insere o usuário na tabela
         cur = conn.cursor()
-        cur.execute("INSERT INTO processos (processo, data, descricao, status) VALUES (%s, %s, %s, %s)", (arq.processo, proc.data, proc.descricao, proc.status))
+        cur.execute("INSERT INTO Arquivo (acod,nome_arquivo, tipo_arq) VALUES (%s, %s, %s)", (arq.acod, arq.nome_arquivo, arq.tipo_arq))
         conn.commit()
-        print("Processo inserido com sucesso!")
+        print("Arquivo inserido com sucesso!")
 
     except psycopg2.Error as e:
-        print("Erro ao inserir processo:", e)
+        print("Erro ao inserir Arquivo:", e)
 
     finally:
         # Fecha a conexão
         if conn is not None:
             conn.close()
 
-def insert_post(post):
+def insert_dados_grafico(dg):
     try:
         # Conecta no banco de dados
         conn = psycopg2.connect(
@@ -262,12 +274,60 @@ def insert_post(post):
 
         # Insere o usuário na tabela
         cur = conn.cursor()
-        cur.execute("INSERT INTO posts (title, datapost, content, status, user_id) VALUES (%s, %s, %s, %s, %s)", (post.title, post.datapost, post.content, post.status, post.user_id))
+        cur.execute("INSERT INTO Dados_Grafico (dgcod, titulo_g, tipo_g, dado_1, dado_2,condicao1,condicao2,acod) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (dg.dgcod, dg.titulo_g, dg.tipo_g, dg.dado_1, dg.dado_2,dg.condicao1,dg.condicao2,dg.acod))
         conn.commit()
-        print("Post inserido com sucesso!")
+        print("dg inserido com sucesso!")
 
     except psycopg2.Error as e:
-        print("Erro ao inserir post:", e)
+        print("Erro ao inserir dg:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+def insert_grafico_coluna(gc):
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Insere o usuário na tabela
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Grafico_coluna (colcod, tituloc_d1, tituloc_d2, dgcodc) VALUES (%s, %s, %s, %s)", (gc.colcod, gc.tituloc_d1, gc.tituloc_d2, gc.dgcodc))
+        conn.commit()
+        print("gc inserido com sucesso!")
+
+    except psycopg2.Error as e:
+        print("Erro ao inserir gc:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+def insert_grafico_barra(gb):
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Insere o usuário na tabela
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Grafico_barra (barcod, titulob_d1, titulob_d2, dgcodb) VALUES (%s, %s, %s, %s)", (gb.barcod, gb.titulob_d1, gb.tipob_d2, gb.dgcodb))
+        conn.commit()
+        print("gb inserido com sucesso!")
+
+    except psycopg2.Error as e:
+        print("Erro ao inserir gb:", e)
 
     finally:
         # Fecha a conexão
@@ -275,7 +335,106 @@ def insert_post(post):
             conn.close()
 
 
-def select_users():
+
+def insert_grafico_pizza(gp):
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Insere o usuário na tabela
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Grafico_pizza (pizcod, titulop_d1, titulop_d2, dgcodp) VALUES (%s, %s, %s, %s)", (gp.pizcod, gp.titulop_d1, gp.titulop_d2, gp.dgcodp))
+        conn.commit()
+        print("gp inserido com sucesso!")
+
+    except psycopg2.Error as e:
+        print("Erro ao inserir gp:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+def insert_grafico_coluna1(gc1):
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Insere o usuário na tabela
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Grafico_coluna1 (col1cod, tituloc_d1, dgcodc1) VALUES (%s, %s, %s)", (gc1.col1cod, gc1.tituloc_d1, gc1.dgcodc1))
+        conn.commit()
+        print("gc1 inserido com sucesso!")
+
+    except psycopg2.Error as e:
+        print("Erro ao inserir gc1:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+def insert_grafico_barra1(gb1):
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Insere o usuário na tabela
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Grafico_barra1 (bar1cod, titulob_d1, dgcodb1) VALUES (%s, %s, %s)", (gb1.bar1cod, gb1.titulob_d1, gb1.dgcodb1))
+        conn.commit()
+        print("gb1 inserido com sucesso!")
+
+    except psycopg2.Error as e:
+        print("Erro ao inserir gb1:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+def insert_grafico_pizza1(gp1):
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Insere o usuário na tabela
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Grafico_pizza1 (piz1cod, titulop_d1, dgcodp1) VALUES (%s, %s, %s)", (gp1.bar1cod, gp1.titulob_d1, gp1.dgcodb1))
+        conn.commit()
+        print("gp1 inserido com sucesso!")
+
+    except psycopg2.Error as e:
+        print("Erro ao inserir gp1:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+            #select
+
+def select_us():
     try:
         # Conecta no banco de dados
         conn = psycopg2.connect(
@@ -287,13 +446,13 @@ def select_users():
 
         # Seleciona todos os usuários da tabela
         cur = conn.cursor()
-        cur.execute("SELECT * FROM users")
+        cur.execute("SELECT * FROM us")
         rows = cur.fetchall()
 
         # Exibe os usuários na tela
         for row in rows:
-            user = User(row[0], row[1], row[2])
-            print(f"ID: {user.id}, Nome: {user.name}, Email: {user.email}")
+            us = Us(row[0], row[1], row[2])
+            print(f"Código: {us.ucod}, Email: {us.email}, Senha: {us.senha}")
 
     except psycopg2.Error as e:
         print("Erro ao selecionar usuários:", e)
@@ -303,7 +462,8 @@ def select_users():
         if conn is not None:
             conn.close()
 
-def select_posts():
+
+def select_arquivo():
     try:
         # Conecta no banco de dados
         conn = psycopg2.connect(
@@ -315,13 +475,13 @@ def select_posts():
 
         # Seleciona todos os usuários da tabela
         cur = conn.cursor()
-        cur.execute("SELECT * FROM posts")
+        cur.execute("SELECT * FROM Arquivo")
         rows = cur.fetchall()
 
         # Exibe os usuários na tela
         for row in rows:
-            post = Post(row[0], row[1], row[2], row[3], row[4], row[5])
-            print(f"ID: {post.id}, Conteúdo: {post.content}, Data: {post.datapost}, Status: {post.status}, User_id: {post.user_id}")
+            arq = Arquivo(row[0], row[1], row[2], row[3], row[4], row[5])
+            print(f"Código: {arq.acod}, Nome: {arq.nome_arquivo}, Tipo: {arq.tipo_arq}")
 
     except psycopg2.Error as e:
         print("Erro ao selecionar usuários:", e)
@@ -332,7 +492,7 @@ def select_posts():
             conn.close()
 
 
-def select_processos():
+def select_Dados_grafico():
     try:
         # Conecta no banco de dados
         conn = psycopg2.connect(
@@ -344,13 +504,181 @@ def select_processos():
 
         # Seleciona todos os usuários da tabela
         cur = conn.cursor()
-        cur.execute("SELECT * FROM processos")
+        cur.execute("SELECT * FROM Dados_Graficos")
         rows = cur.fetchall()
 
         # Exibe os usuários na tela
         for row in rows:
-            proc = Processo(row[0], row[1], row[2], row[3], row[4])
-            print(f"ID: {user.id}, Processo: {proc.descricao}, Data: {proc.data}")
+            dg = Dados_Grafico(row[0], row[1], row[2], row[3], row[4])
+            print(f"Código: {dg.dgcod}, Título gráfico: {dg.titulo_g}, Tipo do Gráfico: {dg.tipo_g}, Dado 1: {dg.dado_1}, Dado 2: {dg.dado_2}, Condição 1: {dg.condicao1}, Condição 2: {dg.condicao2}, Acod: {dg.acod}")
+
+    except psycopg2.Error as e:
+        print("Erro ao selecionar usuários:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+def select_Grafico_coluna():
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Seleciona todos os usuários da tabela
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Grafico_coluna")
+        rows = cur.fetchall()
+
+        # Exibe os usuários na tela
+        for row in rows:
+            gc = Grafico_coluna(row[0], row[1], row[2], row[3], row[4])
+            print(f"Código: {gc.colcod}, Título dado 1: {gc.tituloc_d1}, Tipo dado 2: {gc.tituloc_d2}, Código dado-gráfico: {gc.gcodc}")
+
+    except psycopg2.Error as e:
+        print("Erro ao selecionar usuários:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+def select_Grafico_barra():
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Seleciona todos os usuários da tabela
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Grafico_barra")
+        rows = cur.fetchall()
+
+        # Exibe os usuários na tela
+        for row in rows:
+            gb = Grafico_barra(row[0], row[1], row[2], row[3], row[4])
+            print(f"Código: {gb.barcod}, Título dado 1: {gb.titulob_d1}, Tipo dado 2: {gb.titulob_d2}, Código dado-gráfico: {gb.gcodb}")
+
+    except psycopg2.Error as e:
+        print("Erro ao selecionar usuários:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+def select_Grafico_pizza():
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Seleciona todos os usuários da tabela
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Grafico_pizza")
+        rows = cur.fetchall()
+
+        # Exibe os usuários na tela
+        for row in rows:
+            gp = Grafico_pizza(row[0], row[1], row[2], row[3], row[4])
+            print(f"Código: {gp.pizcod}, Título dado 1: {gp.titulop_d1}, Tipo dado 2: {gp.titulop_d2}, Código dado-gráfico: {gp.dgcodp}")
+
+    except psycopg2.Error as e:
+        print("Erro ao selecionar usuários:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+def select_Grafico_coluna1():
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Seleciona todos os usuários da tabela
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Grafico_coluna1")
+        rows = cur.fetchall()
+
+        # Exibe os usuários na tela
+        for row in rows:
+            gc1 = Grafico_coluna1(row[0], row[1], row[2], row[3], row[4])
+            print(f"Código: {gc1.col1cod}, Título dado 1: {gc1.tituloc_d1}, Código dado-gráfico: {gc1.dgcodc1}")
+
+    except psycopg2.Error as e:
+        print("Erro ao selecionar usuários:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+def select_Grafico_barra1():
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Seleciona todos os usuários da tabela
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Grafico_barra1")
+        rows = cur.fetchall()
+
+        # Exibe os usuários na tela
+        for row in rows:
+            gb1 = Grafico_barra1(row[0], row[1], row[2], row[3], row[4])
+            print(f"Código: {gb1.bar1cod}, Título dado 1: {gb1.titulob_d1}, Código dado-gráfico: {gb1.dgcodb1}")
+
+    except psycopg2.Error as e:
+        print("Erro ao selecionar usuários:", e)
+
+    finally:
+        # Fecha a conexão
+        if conn is not None:
+            conn.close()
+
+def select_Grafico_pizza1():
+    try:
+        # Conecta no banco de dados
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DATA_BASE,
+            user=USER,
+            password=PASS
+        )
+
+        # Seleciona todos os usuários da tabela
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Grafico_pizza1")
+        rows = cur.fetchall()
+
+        # Exibe os usuários na tela
+        for row in rows:
+            gp1 = Grafico_pizza1(row[0], row[1], row[2], row[3], row[4])
+            print(f"Código: {gp1.piz1cod}, Título dado 1: {gp1.titulop_d1}, Tipo dado 2: {gp1.tituloc_d2}, Código dado-gráfico: {gp1.dgcodp1}")
 
     except psycopg2.Error as e:
         print("Erro ao selecionar usuários:", e)
